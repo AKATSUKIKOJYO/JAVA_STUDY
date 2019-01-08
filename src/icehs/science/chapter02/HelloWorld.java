bb073bfa8dc8536721cb6c285f93d932dc22181f
admin <admin@10.137.138.115>
2019-01-08 18:10:40 +0900

    Update3

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -3,9 +3,15 @@ package icehs.science.chapter02;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		/*
+		 * 처음만든 클래스
+		 * 제작자: 김인규
+		 * 제작일: 2019.01.08
+		 * 클래스의 목적: Dennis Richie를 기념하기 위한 명령어
+		 */
 		System.out.println("Hello World");
 		System.out.println("안녕하세요?");
-		System.out.println("Goodbye teen");
+		System.out.println("Goodbye teenager");
 		
 	}
 
