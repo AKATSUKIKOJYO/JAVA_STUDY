da0fe4371e4813b001755c512220f640354629f2
admin <admin@10.137.138.115>
2019-01-17 19:07:06 +0900

    WORKSHOP03

---
+++ src/icehs/science/workshop2/Account.java
@@ -0,0 +1,67 @@
+package icehs.science.workshop2;
+
+public class Account {
+	private String account;
+	private int balance;
+	private double interestRate;
+	
+	public Account() {
+		
+	}
+	
+	public Account(String account, int balance, double interestRate) {
+		this.account = account;
+		this.balance = balance;
+		this.interestRate = interestRate;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public void deposit(int money) {
+		this.balance += money;
+		System.out.println(">>" + money + "원을 입금합니다.");
+		System.out.println("계좌번호 : " + this.account + ", 잔액 : " + this.balance + ", 이자율 : " + this.interestRate + "%");
+	}
+	
+	public void withdraw(int money) {
+		this.balance -= money;
+		System.out.println(">>" + money + "원을 출금합니다.");
+		System.out.println("계좌번호 : " + this.account + ", 잔액 : " + this.balance + ", 이자율 : " + this.interestRate + "%");
+	}
+	
+	public void calculateInterst() {
+		int price = (int)(this.balance * this.interestRate / 100);
+		this.balance += price;
+		System.out.println("이자를 지급합니다.");
+		System.out.println("이자 : " + price + "원");
+		System.out.println("계좌번호 : " + this.account + ", 잔액 : " + this.balance + ", 이자율 : " + this.interestRate + "%");
+	}
+	
+	public void printAccountInfo() {
+		System.out.println(">>계좌를 생성합니다.");
+		System.out.println("계좌번호 : " + this.account + ", 잔액 : " + this.balance + ", 이자율 : " + this.interestRate + "%");
+	}
+	
+}
---
+++ src/icehs/science/workshop2/AccountTest.java
@@ -0,0 +1,19 @@
+package icehs.science.workshop2;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+		Account account = new Account("441-0290-1203", 500000, 7.3);
+		account.printAccountInfo();
+		System.out.println();
+		account.deposit(20000);
+		System.out.println();
+		account.calculateInterst();
+		System.out.println();
+		account.withdraw(150000);
+		System.out.println();
+		account.withdraw(500000);
+
+	}
+
+}
